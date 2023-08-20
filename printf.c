43676dfedfb83203c4724abb967bb8760913439b
7034megatron <emmanuemabhena0@gmail.com>
2023-08-20 07:35:22 -0700

    initial draft

---
+++ printf.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
+
+int _putchar(char c) {
+    return (write(1, &c, 1));
+}
+
+int _print(const char* format, ...) {
+
+    va_list args;
+    int charsPrinted = 0;
+    va_start(args, format);
+
+    // printf('Hello world, %c %s"", 'a', "string")
+
+    while(*format != '%0') {
+            if (*format == '%') {
+                    // increment the pointer first
+                    format++;
+                    switch(*format) {
+                    case 'c':
+                        {
+                             int c = va_arg(args, int);
+                            _putchar(c);
+                            charsPrinted++;
+                            format++;
+                        }
+
+                        break;
+                    case 's':
+                        {
+                            char *str = va_arg(args, char*);
+                            while(*str != "\0") {
+                                charsPrinted++;
+                                _putchar(*str);
+                                str++;
+                            }
+                            format++;
+                        }
+
+                        break;
+                    default:
+                        _putchar(*format);
+                        charsPrinted++;
+                    }
+
+            }else {
+                charsPrinted++;
+                // print char to std out
+                _putchar(*format); // What if this errors out
+                // increment pointer
+                format++;
+            }
+    }
+
+
+    return charsPrinted;
+}
